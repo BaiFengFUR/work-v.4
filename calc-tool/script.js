a2851de3facbb24846da376b50ad33514500cd07
BaiFeng <2104043918@qq.com>
2026-09-11 17:18:09 +0800

    序号2

---
+++ calc-tool/script.js
@@ -0,0 +1,11 @@
+// 消费记账数据
+const expenses = [
+  { category: '餐饮', amount: 35, note: '午餐' },
+  { category: '交通', amount: 12, note: '地铁' },
+  { category: '餐饮', amount: 28, note: '晚餐' },
+  { category: '购物', amount: -50, note: '退款误录' },
+  { category: '交通', amount: 15, note: '打车' },
+  { category: '餐饮', amount: 0, note: '朋友请客' },
+  { category: '娱乐', amount: 88, note: '电影票' },
+  { category: '购物', amount: 9999, note: '测试异常' }
+];
